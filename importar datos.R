c06988a9b910dc51c60786fb94e863d4ed04a223
diegotrascasa <dtg1003@alu.ubu.es>
2022-11-14 10:08:40 +0100

    importar nuevos datos

--- importar datos.R
+++ importar datos.R
@@ -1,9 +1,10 @@
-library(readr)
-X46687 <- read_delim("46687.csv", delim = "\t", 
-                     escape_double = FALSE, trim_ws = TRUE)
-View(X46687)
-
-
 library(readxl)
-datos_aire_20220 <- read_excel("~/Downloads/datos_aire_20220.xlsx")
+datos_aire_20220 <- read_excel("input/data/datos_aire_20220.xlsx")
 View(datos_aire_20220)
+
+
+library(readr)
+X02001 <- read_delim("input/data/02001.csv", 
+                     delim = "\t", escape_double = FALSE, 
+                     trim_ws = TRUE)
+View(X02001)
