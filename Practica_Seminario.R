9485e4f7176f5510744d0f419801634ef729f91f
diegotrascasa <dtg1003@alu.ubu.es>
2022-11-19 18:46:34 +0100

    limpiar de los anteriores datos

--- Practica_Seminario.R
+++ Practica_Seminario.R
@@ -4,8 +4,3 @@
 
 library(dplyr)
 
-datos_aire_20220b <- rename(.data = datos_aire_20220, Comunidad_Autonoma = Estación)
-colnames(X02001) <- c("Sexo","Diagnóstico principal","Total Nacional","Comunidad_Autonoma","Provincia","Total")
-View(X02001)
-datos_aire_20220C <- mutate(.data = datos_aire_20220b, Comunidad_Autonoma = " Castilla y León")
-View(datos_aire_20220C)
