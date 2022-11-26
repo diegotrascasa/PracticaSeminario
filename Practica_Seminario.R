8a0864fccdbef9f9aa12dfaaeb87a41cae777fda
diegotrascasa <dtg1003@alu.ubu.es>
2022-11-26 19:38:04 +0100

    Media de todos los años por provincia

--- Practica_Seminario.R
+++ Practica_Seminario.R
@@ -3,4 +3,10 @@
 #Ajustar tablas para que coincidan las columnas:
 
 library(dplyr)
+DW1 <-
+select(.data = calidadFinal,PROVINCIA:ARSENICO)%>%
+  group_by(PROVINCIA)%>%
+  summarise(across(c(ARSENICO), ~ mean(.x, na.rm = TRUE)))
+  print(DW1)
+  
 
